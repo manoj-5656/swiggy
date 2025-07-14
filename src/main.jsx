534a7c8f8fb4e7b9a65212367d7d3cd18d5ed68b
manoj-5656 <kathimanojreddy@gmail.com>
2025-07-14 12:46:58 +0530

    swiggy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // ✅ Use correct case
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App'
-
-createRoot(document.getElementById('root')).render(< App/> )
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
